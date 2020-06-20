7f8e3da45da075373ec9ebd38db9ebac98810c79
Andrea Bruno <andybbruno@gmail.com>
2020-06-20 09:28:49 +0200

    Fixed plots and some filenames

--- plot_lz_tz.py
+++ plot_lz_tz.py
@@ -3,14 +3,17 @@ import glob
 import pandas as pd
 from pylab import *
 import matplotlib.pyplot as plt
+import random
 
 path = 'res_lz_tz'
-
 
 v = 0
 i = 0
 fig, axs = plt.subplots(nrows=7, ncols=2, sharex=True)
 
+random.seed(0)
+all_colors = list(plt.cm.colors.cnames.keys())
+c = random.choices(all_colors, k=8)
 
 pathlist = sort(os.listdir(path))
 
@@ -22,8 +25,12 @@ for filename in pathlist:
         x = df['i']
         y = df['freq']
 
-        axs[v, i].bar(x, y)
-        axs[v, i].set_title(filename)
+        axs[v, i].bar(x, y, color=c, edgecolor='lightgray')
+        axs[v, i].ticklabel_format(style="sci", scilimits=(0, 0), axis="y")
+
+        title = filename.split('.')[0].split('_')[0] + " " + filename.split('.')[0].split('_')[1]
+        axs[v, i].set_title(title)
+        axs[v, i].grid(axis='x', linestyle=':')
 
         if i == 0:
             i = i + 1
@@ -31,10 +38,10 @@ for filename in pathlist:
             v = v + 1
             i = 0
 
+subplots_adjust(left=0.3, bottom=0.05, right=0.7,
+                top=0.95, wspace=None, hspace=0.6)
 
-subplots_adjust(left=0.05, bottom=0.05, right=0.95,
-                top=0.95, wspace=None, hspace=0.4)
-
+plt.xticks(x)
 plt.show()
 # plt.savefig('OFFSET_IMG/' + filename + '.png', dpi=300)
 # plt.close()
