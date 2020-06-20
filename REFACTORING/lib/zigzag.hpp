8a017a544b9411aad075a552558ceaeb9244478b
Andrea Bruno <andybbruno@gmail.com>
2020-06-20 10:48:28 +0200

    Start refactoring

---
+++ REFACTORING/lib/zigzag.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <utility>
+
+namespace zz
+{
+inline uint64_t encode(int64_t i)
+{
+    return (i >> 63) ^ (i << 1);
+}
+
+inline int64_t decode(uint64_t i)
+{
+    return (i >> 1) ^ (-(i & 1));
+}
+} // namespace zigzag
