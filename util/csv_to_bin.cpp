05205e7d26f64929ca2523b870ccea08f2e2077f
Andrea Bruno <andybbruno@gmail.com>
2020-06-24 18:44:12 +0200

    Bugfix

---
+++ util/csv_to_bin.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include "CSVReader.cpp"
+
+int main(int argc, char *argv[])
+{
+    std::string filename(argv[1]);
+    CSVReader reader(filename);
+
+    uint64_t nlines = 0;
+    std::vector<std::vector<double>> lines;
+    while (!reader.isEmpty())
+    {
+        lines.push_back(reader.nextLine());
+        nlines++;
+    }
+    uint64_t ncols = lines[0].size();
+
+    
+    std::string rawname = filename.substr(0, filename.find_last_of("."));
+    filename = rawname + std::string(".bin");
+
+    auto myfile = std::ofstream(filename, std::ios::out | std::ios::binary);
+
+    char *c_nlines = (char *)&nlines;
+    char *c_ncols = (char *)&ncols;
+
+    myfile.write(c_nlines, sizeof(uint64_t));
+    myfile.write(c_ncols, sizeof(uint64_t));
+
+    for (int i = 0; i < nlines; i++)
+    {
+        for (int j = 0; j < ncols; j++)
+        {
+            myfile.write((char *)&lines[i][j], sizeof(double));
+        }
+    }
+
+    myfile.close();
+
+    std::cout << "File successfully converted!" << std::endl;
+    std::cout << filename << std::endl;
+}
